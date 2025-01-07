051fbf4deeb819b04ad6551a58827f046e9ed1fa
Kyle Finkernagel <43211129+kf1232@users.noreply.github.com>
2025-01-06 20:55:36 -0600

    more icons for factions.

--- src/app/data/factions/faction/cephalonSuda.tsx
+++ src/app/data/factions/faction/cephalonSuda.tsx
@@ -7,7 +7,17 @@ const CEPHALON_SUDA = new FactionObject(
     5,
     true,
     ['arbitersOfHexis'],
-    ['redVeil', 'newLoka']
+    ['redVeil', 'newLoka'],
+    {
+        [-2]: '/images/factionImages/cephalonSuda/0.webp',
+        [-1]: '/images/factionImages/cephalonSuda/1.webp',
+        [0]:  '/images/factionImages/cephalonSuda/2.webp',
+        [1]:  '/images/factionImages/cephalonSuda/3.webp',
+        [2]:  '/images/factionImages/cephalonSuda/4.webp',
+        [3]:  '/images/factionImages/cephalonSuda/5.webp',
+        [4]:  '/images/factionImages/cephalonSuda/6.webp',
+        [5]:  '/images/factionImages/cephalonSuda/7.webp',
+    }
 );
 
 export default CEPHALON_SUDA;
